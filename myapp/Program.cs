faef6c5e69d9e8970d58babdc6461097b6e99afd
Takanori_Saito <tsaito@matehan.co.jp>
2022-03-09 22:40:12 +0900

    first commit

---
+++ myapp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using myapp.lib;
+
+namespace myapp
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new calculator().Add(10,1);
+
+            Console.WriteLine($"Hello World! c => {c}");
+        }
+    }
+}
---
+++ myapp/lib/util.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace myapp.lib
+{
+   internal class calculator
+    {
+        internal int Add(int x, int y) => x + y;
+    }
+}
